6b30c52e922fd5b5a8a54de7bf50d36907e0515b
bertyjobbo <robjohnson1978@gmail.com>
2014-10-13 07:47:59 +0100

    Started cview

---
+++ AltNg/Views/AltCView/Scripts/alt.altcviewapp.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+var alt = alt || {};
+alt.altcviewapp = angular.module("alt.altcviewapp", ["ng", "alt.croute"]);
+(function(app) {
+
+    app.config(["altCRouteConfigProvider", function(altCRouteConfigProvider) {
+
+        altCRouteConfigProvider
+            .mapRoute("Default",
+				"{controller}/{action}",
+				new { controller: "home", action: "index" },
+				function (c, a) { return "AltCView/NgView?c=" + c + "&a=" + a; })
+            .mapRoute("Alternative",
+				"Alternative/{controller}/{action}",
+				new { controller: "home", action: "index" },
+				function (c, a) { return "AltCView/NgView?c=" + c + "&a=" + a; });
+
+    }]);
+
+})(alt.altcviewapp);
